6f74542f6009388c8c1fd08b32352bf71ae816ea
leo-jesus <llxdgb1@gmail.com>
2022-04-24 23:17:08 -0300

    add styling and some functions

--- public/script.js
+++ public/script.js
@@ -1,10 +1,16 @@
 const socket = io("/");
-
 const videoGrid = document.getElementById("video-grid");
 const video = document.createElement("video");
 video.muted = true;
 
+const peer = new Peer(undefined, {
+  path: "/peerjs",
+  port: "4000",
+  host: "/",
+});
+
 let streamVideo;
+
 navigator.mediaDevices
   .getUserMedia({
     video: true,
@@ -13,16 +19,47 @@ navigator.mediaDevices
   .then((stream) => {
     streamVideo = stream;
     addVideoStream(video, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userID) => {
+      connectToNewUser(userID, stream);
+    });
+
+    let text = $("input");
+
+    $("html").keydown((e) => {
+      if (e.which == 13 && text.val().length !== 0) {
+        console.log(text.val());
+        socket.emit("message", text.val());
+        text.val("");
+      }
+    });
+
+    socket.on("createMessage", (message) => {
+      $(".messages").append(
+        `<li class="message"><b>user</b><br/>${message}</li>`,
+        scrollToBottom(),
+      );
+    });
   });
 
-socket.emit("join-room", ROOM_ID);
-
-socket.on("user-connected", () => {
-  connectToNewUser();
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
 });
 
-const connectToNewUser = () => {
-  console.log("new user");
+const connectToNewUser = (userID, stream) => {
+  const call = peer.call(userID, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
 };
 
 const addVideoStream = (video, stream) => {
@@ -31,4 +68,9 @@ const addVideoStream = (video, stream) => {
     video.play();
   });
   videoGrid.append(video);
+};
+
+const scrollToBottom = () => {
+  let d = $(".main__chat_widow");
+  d.scrollTop(d.prop("scrollHeight"));
 };
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { Socket } = require("socket.io");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 const { v4: uuidv4 } = require("uuid");
+
+app.use("/peerjs", peerServer);
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
@@ -17,10 +23,15 @@ app.get("/:room", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  socket.on("join-room", (roomID) => {
+  socket.on("join-room", (roomID, userID) => {
     socket.join(roomID);
-    socket.to(roomID).emit("user-connected");
+    socket.to(roomID).emit("user-connected", userID);
+    socket.on("message", (message) => {
+      io.to(roomID).emit("createMessage", message);
+    });
   });
 });
 
-server.listen(4000);
+server.listen(4000, () => {
+  console.log("server running at 4000");
+});
